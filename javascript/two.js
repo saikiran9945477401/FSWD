938111d08b17981d2f7b7a7c0200804651d87f6c
saikiran9945477401 <saikiranj001@gmail.com>
2021-08-15 15:04:14 +0530

    ss

--- javascript/two.js
+++ javascript/two.js
@@ -3,4 +3,4 @@ const num2 = parseFloat(prompt("Enter the second number: "));
 const num3 = parseFloat(prompt("Enter the third number: "));
 
 const largest = Math.max(num1,num2,num3);
-console.log("the largest number is" +largest);
+console.log(" the largest number is" +largest);
