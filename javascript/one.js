7fa2ee6915459da884dfba718bd4729a1a6f8558
saikiran9945477401 <saikiranj001@gmail.com>
2021-08-15 10:42:05 +0530

    ss

---
+++ javascript/one.js
@@ -0,0 +1,14 @@
+/*check number is divisible by 7*/
+function number(x)
+{
+if(x % 7 ==0){
+    return true;
+}
+else{
+   return false;
+}
+}
+console.log(number(10));
+console.log(number(35));
+console.log(number(42));
+console.log(number(28));
