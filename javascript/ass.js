a0507b7bf56172f19cfedc50a55908928246007c
saikiran9945477401 <saikiranj001@gmail.com>
2021-08-21 10:44:47 +0530

    commit

--- javascript/ass.js
+++ javascript/ass.js
@@ -1,5 +1,4 @@
-var numarray=[56,65,76,45,34,44,56,];
-numarray.sort(function num(a,b){
-return a-b;
-});
+/*desending order*/
+let numarray=[56,65,76,45,34,44,56,];
+numarray.sort((a,b)=> b-a);
 console.log(numarray);
