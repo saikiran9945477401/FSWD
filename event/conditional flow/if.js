da8ad7798cec213b74304abacff34a49e3f3ee4e
saikiran9945477401 <saikiranj001@gmail.com>
2021-09-05 15:35:03 +0530

    case

---
+++ event/conditional flow/if.js
@@ -0,0 +1,24 @@
+let day=new Date().getDay();
+switch(day){
+case 0 :
+    console.log("sun");
+    break;
+    case 1:
+        console.log("mon");
+        break; 
+        case 2 :
+    console.log("tue");
+    break;
+    case 3:
+        console.log("wed");
+        break;
+        case 4:
+    console.log("thu");
+    break;
+    case 5:
+        console.log("fri");
+        break;
+        case 6 :
+    console.log("sat");
+    break;
+    }
